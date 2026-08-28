7cc7588dc9bed3b2384bf33b9302026d06d56c66
ARTHUR ROMUALDO SANTOS B <arthur.rsb@senacsp.edu.br>
2026-08-28 20:51:02 -0300

    Commit inicial do projeto InfinityPart

---
+++ InfinityPart/Entidades/PerfilUsuario.cs
@@ -0,0 +1,20 @@
+﻿namespace InfinityPart.Domain.Entities
+{
+    public class PerfilUsuario : IdentityUser
+    {
+        public required string Nome { get; set; }
+        public int Cpf { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public string? FotoPerfil { get; set; }
+        public bool Ativo { get; set; } = true;
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+        public DateTime? DataAtualizacao { get; set; }
+        public DateTime? DataExclusao { get; set; }
+
+
+
+
+
+
+    }
+}
